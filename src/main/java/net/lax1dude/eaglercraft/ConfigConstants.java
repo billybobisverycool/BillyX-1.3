50dbb56223033936543544ea4eca31a895ff3e65
billybobisverycool <27johnballenger@ucsstudent.org>
2023-05-12 08:58:06 -0400

    Update ConfigConstants.java

--- src/main/java/net/lax1dude/eaglercraft/ConfigConstants.java
+++ src/main/java/net/lax1dude/eaglercraft/ConfigConstants.java
@@ -5,9 +5,9 @@ public class ConfigConstants {
 	public static boolean profanity = false;
 
 	public static final String version = "22w22b";
-	public static final String mainMenuString = "eaglercraft beta-" + version;
+	public static final String mainMenuString = "BillyX1.3-" + version;
 
-	public static final String forkMe = "https://github.com/LAX1DUDE/eaglercraft";
+	public static final String forkMe = "https://github.com/billybobisverycool/BillyX-1.3";
 
 	public static final boolean html5build = true;
 
